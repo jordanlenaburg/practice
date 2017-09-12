2498d77e43ff083b1de058d41841619d945b424b
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-09-12 12:19:21 -0600

    added new problems

--- euler.js
+++ euler.js
@@ -3,7 +3,7 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 const multiples = () => {
     let sum = 0;
-    for (i = 0; i < 1000; i++){
+    for (let i = 0; i < 1000; i++){
         if (i % 3 === 0 || i % 5 === 0) {
             sum = sum + i;
         }
@@ -17,7 +17,7 @@ const multiples = () => {
 const evenFib = () => {
     let sum = 2;
     let sequence = [1, 2, 3];
-    for (i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
         let currentNum = (sequence[sequence.length-2] + sequence[sequence.length-1]);
         if (currentNum >= 4000000) {
             break;
@@ -38,7 +38,7 @@ const primeFactor = (num) => {
     let factors = [];
     let count = 2;
     let remainingMultiple = num;
-    for (var i = 0; i < 100000; i++) {
+    for (let i = 0; i < 100000; i++) {
         if (remainingMultiple % count === 0) {
             // console.log(remainingMultiple)
             remainingMultiple = remainingMultiple / count;
@@ -55,8 +55,8 @@ const primeFactor = (num) => {
 // Find the largest palindrome made from the product of two 3-digit numbers.
 const palindrome = () => {
     let number = 0;
-    for (i = 100; i < 1000; i++) {
-        for (j = 100; j < 1000; j++) {
+    for (let i = 100; i < 1000; i++) {
+        for (let j = 100; j < 1000; j++) {
             let product = (i * j);
             let string = product.toString();
             let array = string.split("");
@@ -67,14 +67,67 @@ const palindrome = () => {
     }
     return number;
 };
-// UNSOLVED
+// SOLVED
 // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-const evenDivisor = () => {
-    let count = 1;
-    for (0; i < 20; i++) {
-        if (count % i != 0) {
-            count++
+// Recursively, this might exceed the maximum call stack
+///////////////////////////////////////////
+// const evenDivisorRecursive = (divisor, num) => {
+//     // if divisor reaches 0, return num
+//     if (divisor <= 0){
+//         return num;
+//     }
+//     // try num % divisor, if 0, decrease divisor by 1
+//     else if (num % divisor === 0) {
+//         return evenDivisor(divisor-1, num)
+//     }
+//     // if not 0, increase num, reset divisor
+//     else {
+//         return evenDivisor(20, num+1)
+//     }
+// };
+// Non-recursive
+///////////////////////////////////////////
+const evenDivisor = (divisor) => {
+    // establish current number
+    let num = 1;
+    // establish for loop with divisor as index tracker
+    for (let i = divisor; i > 0; i--) {
+        // check if number % divisor (marked as i) === 0
+        if (num % i !== 0){
+            // if not, increase current number, reset divisor
+            num++
+            i = divisor
         }
+        // if so, got to next iteration
     }
+    return num;
 };
+
+// UNSOLVED
+// The sum of the squares of the first ten natural numbers is,
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+//     (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+const sumSquareDifference = (num) => {
+    //establish variables
+    let sum = 0;
+    let square = 0;
+    //establish for loop to iterate through num
+    for (var i = 1; i <= num; i++){
+        // sum of squares
+        sum += Math.pow(i, 2);
+        // sum
+        square += i;
+    }
+    // square the sum
+    square = Math.pow(square, 2);
+    // return difference
+    return square - sum;
+
+};
+
+console.log(sumSquareDifference(100));
